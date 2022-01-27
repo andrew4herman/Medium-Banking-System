3a94c6e83c04ed96ef4c2bc2dfed734cd2cdf7ef
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-27 14:25:10 +0200

    Remove unnecessary usage of Optional in Main class

--- src/main/java/banking/Main.java
+++ src/main/java/banking/Main.java
@@ -5,30 +5,25 @@ import banking.database.DBManager;
 import banking.util.CardGenerator;
 import banking.util.CardValidator;
 import banking.util.CliParser;
-
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         String BINumber = "400000";
-        Optional<String> fileName = new CliParser(args).optionOf("-fileName");
+        String fileName = new CliParser(args).optionOf("-fileName")
+                .orElseThrow(() -> new IllegalArgumentException("Incorrect option for -fileName"));
 
-        if (fileName.isPresent()) {
-            DBManager dbManager = new DBManager(fileName.get());
-            AccountDao accountDao = new AccountDao(dbManager);
+        DBManager dbManager = new DBManager(fileName);
+        AccountDao accountDao = new AccountDao(dbManager);
 
-            CardValidator cardValidator = new CardValidator();
-            CardGenerator cardGenerator = new CardGenerator(cardValidator, BINumber);
+        CardValidator cardValidator = new CardValidator();
+        CardGenerator cardGenerator = new CardGenerator(cardValidator, BINumber);
 
-            Bank bank = new Bank(accountDao, cardValidator, cardGenerator);
-            BankingSystem system = new BankingSystem(bank, new Scanner(System.in));
+        Bank bank = new Bank(accountDao, cardValidator, cardGenerator);
+        BankingSystem system = new BankingSystem(bank, new Scanner(System.in));
 
-            system.start();
-            dbManager.closeConnection();
-        } else {
-            throw new IllegalArgumentException("Incorrect option for -fileName");
-        }
+        system.start();
+        dbManager.closeConnection();
     }
 }
